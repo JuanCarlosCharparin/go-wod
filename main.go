760027b53ea0834783aeb50ef3297c1e801da4a1
JuanCarlosCharparin <jccharparin@gmail.com>
2025-12-18 21:15:19 -0300

    last commit

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func main() {
 
 	// Configuración explícita de CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8081"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
